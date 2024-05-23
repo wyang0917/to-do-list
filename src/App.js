b203e6a2b615e792f35380550fc2b1657e7d806e
wyang0917 <yangwang0917@gmail.com>
2024-05-23 14:22:25 +1000

    formate app.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,81 @@
-import React,{useState} from "react"
-import {v4 as uuidv4} from 'uuid'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
-import TodoInputForm from "./components/TodoInputForm";
-import TodoList from "./components/TodoList";
-import TodoFilter from "./components/TodoFilter"
-import TodoCompletedList from "./components/TodoCompletedList";
-import TodoNotCompletedList from "./components/TodoNotCompletedList";
-
+import TodoInputForm from './components/TodoInputForm';
+import TodoList from './components/TodoList';
+import TodoFilter from './components/TodoFilter';
+import TodoCompletedList from './components/TodoCompletedList';
+import TodoNotCompletedList from './components/TodoNotCompletedList';
 
 function App() {
-  const [todos,setTodos] = useState([])
-  const [completed,setCompleted] = useState(false)
-  const [notCompleted,setNotCompleted] =useState(false)
+  const [todos, setTodos] = useState([]);
+  const [completed, setCompleted] = useState(false);
+  const [notCompleted, setNotCompleted] = useState(false);
 
-  const addTodo=(text)=>{
-    const newTodo = {id:uuidv4(),text,isCompleted:false}
-    setTodos([...todos,newTodo])
-  }
+  const addTodo = (text) => {
+    const newTodo = { id: uuidv4(), text, isCompleted: false };
+    setTodos([...todos, newTodo]);
+  };
 
-  const toggleTodo=(id)=>{
-    const newTodos = todos.map(todo=>todo.id===id ? {...todo,isCompleted:!todo.isCompleted} : todo)
-    setTodos(newTodos);
-  }
-
-  const removeTodo = (idToRemove) => {
-    const newTodos = todos.filter((todo)=>todo.id!==idToRemove)
+  const toggleTodo = (id) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
     setTodos(newTodos);
   };
-  const editTodo = (idToEdit)=>{
-    const editTodo = todos.find((todo)=>todo.id===idToEdit)
-    const editText = prompt('Please edit your todo',editTodo.text)
-    if(editText){
-      const updateTodo = {...editTodo,text:editText}
-      const updateTodos = todos.map((todo)=>todo.id===idToEdit ? updateTodo : todo)
-    setTodos(updateTodos)
+
+  const removeTodo = (idToRemove) => {
+    const newTodos = todos.filter((todo) => todo.id !== idToRemove);
+    setTodos(newTodos);
+  };
+  const editTodo = (idToEdit) => {
+    const editTodo = todos.find((todo) => todo.id === idToEdit);
+    const editText = prompt('Please edit your todo', editTodo.text);
+    if (editText) {
+      const updateTodo = { ...editTodo, text: editText };
+      const updateTodos = todos.map((todo) =>
+        todo.id === idToEdit ? updateTodo : todo
+      );
+      setTodos(updateTodos);
     }
-    
-  }
-  const showCompleted = ()=>{
-    setCompleted(true)
-  }
-  const hideCompleted = ()=>{
-    setCompleted(false)
-  }
-  const showNotCompleted = ()=>{
-    setNotCompleted(true)
-  }
-  const hideNotCompleted = ()=>{
-    setNotCompleted(false)
-  }
+  };
+  const showCompleted = () => {
+    setCompleted(true);
+  };
+  const hideCompleted = () => {
+    setCompleted(false);
+  };
+  const showNotCompleted = () => {
+    setNotCompleted(true);
+  };
+  const hideNotCompleted = () => {
+    setNotCompleted(false);
+  };
   return (
     <div className="App">
       <h1 className="appHeader">TodoList</h1>
       <TodoInputForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} removeTodo={removeTodo} editTodo={editTodo}/>
-      <TodoFilter showCompleted={showCompleted} showNotCompleted={showNotCompleted}/>
-      {completed && <TodoCompletedList hideCompleted={hideCompleted} todos={todos}/>}
-      {notCompleted && <TodoNotCompletedList hideNotCompleted={hideNotCompleted} todos={todos}/>}
+      <TodoList
+        todos={todos}
+        toggleTodo={toggleTodo}
+        removeTodo={removeTodo}
+        editTodo={editTodo}
+      />
+      <TodoFilter
+        showCompleted={showCompleted}
+        showNotCompleted={showNotCompleted}
+      />
+      {completed && (
+        <TodoCompletedList hideCompleted={hideCompleted} todos={todos} />
+      )}
+      {notCompleted && (
+        <TodoNotCompletedList
+          hideNotCompleted={hideNotCompleted}
+          todos={todos}
+        />
+      )}
     </div>
-  )
-  
+  );
 }
 
 export default App;
